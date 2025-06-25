4711d9b2472ab3391baad2f7be0b59dba21f9ea6
Safavereau <126572161+Safavereau@users.noreply.github.com>
2025-06-25 17:40:46 -0300

    Products actions

---
+++ src/app/api/producto/crearProducto/route.ts
@@ -0,0 +1,18 @@
+import { createProduct } from "@/actions/productos"; // ✅ ruta correcta
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const result = await createProduct(body);
+
+    if (result.error) {
+      return NextResponse.json({ error: result.error }, { status: 400 });
+    }
+
+    return NextResponse.json(result);
+  } catch (err) {
+    console.error("API error:", err);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
