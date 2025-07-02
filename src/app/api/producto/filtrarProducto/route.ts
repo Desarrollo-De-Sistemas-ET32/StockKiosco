4cc026ae3f4e03695cbeb794a76dedefdda30dfe
Benjamin <benjamin.castromadrid28et32@gmail.com>
2025-07-01 23:47:34 -0300

    Funcionalidades Usuario
    Se realizan varías funcionalidades para el apartado del usuario.

---
+++ src/app/api/producto/filtrarProducto/route.ts
@@ -0,0 +1,18 @@
+import { filterProduct } from "@/actions/filtrarProducto";
+import { NextResponse } from "next/server";
+
+export async function POST(req: Request) {
+  try {
+    const body = await req.json();
+    const result = await filterProduct(body);
+
+    if (result.error) {
+      return NextResponse.json({ error: result.error }, { status: 400 });
+    }
+
+    return NextResponse.json(result);
+  } catch (err) {
+    console.error("API error:", err);
+    return NextResponse.json({ error: "Internal Server Error" }, { status: 500 });
+  }
+}
