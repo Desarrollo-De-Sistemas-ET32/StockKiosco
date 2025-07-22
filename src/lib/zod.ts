fb64a7e80aae6b5ee5faa516b02d253bc5e4b04c
Benjamin <benjamin.castromadrid28et32@gmail.com>
2025-07-22 15:30:17 -0300

    Se actualiza el zod.ts
    Se actualiza el zod.ts para cumplir con el CRUD de usuarios.

--- src/lib/zod.ts
+++ src/lib/zod.ts
@@ -23,28 +23,24 @@ export const registerSchema = object({
     .max(32, "Password must be less than 32 characters")
 
 })
-
-export const updateUserSchema = z.object({
-  id_proveedor: z.number().int().positive(),
-  email: string({ required_error: "Ingrese un email" })
-    .min(1, "Ingrese un email")
-    .email("Email invalido"),
-  name: string({ required_error: "Ingrese un nombre" })
-    .min(1, "Ingrese un nombre")
-    .max(32, "El nombre debe tener menos de 32 caracteres"),
-  password: string({ required_error: "Se necesita una contraseña" })
-    .min(1, "Se necesita una contraseña")
-    .min(8, "La contraseña debe tener más de 8 caracteres")
-    .max(32, "La contraseña debe tener menos de 32 caracteres"),
-  usuario_roles: z.string().optional(),
-  fecha_actualizacion: z.preprocess((arg) => {
-      if (typeof arg === "string" || arg instanceof Date) return new Date(arg);
-    }, z.date({ required_error: "Fecha inválida" }))
-});
+// Borrar usuario
 
 export const delUserSchema = object({
-  nombre: string({ required_error: "Se necesita un nombre" })
-    .min(1, "Se necesita un nombre")
-    .min(8, "El nombre debe tener más de 8 caracteres")
-    .max(32, "El nombre debe tener menos de 32 caracteres")
+  email: string({ required_error: "Email is required" })
+    .min(1, "Email is required")
+    .email("Invalid email"),
+  password: string({ required_error: "Password is required" })
+    .min(1, "Password is required")
+    .min(8, "Password must be more than 8 characters")
+    .max(32, "Password must be less than 32 characters"),
+})
+
+// Actualizar usuario
+
+export const updateUserSchema = object({
+  email: string({required_error: "Se necesita un email"}),
+  nombre: string({required_error: "Se necesita un nombre"}),
+  telefono: string().optional().default(""),
+  direccion: string().optional().default(""),
+  fecha_actualizacion: z.date().optional().default(new Date())
 })
