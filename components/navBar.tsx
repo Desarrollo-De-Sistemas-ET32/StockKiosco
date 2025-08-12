4c3c9f3c2d7fccb8de7d895edf02ff3836f78a17
Sebas <sebastian.deleon.et32@gmail.com>
2025-08-12 15:14:37 -0300

    si

--- components/navBar.tsx
+++ components/navBar.tsx
@@ -10,31 +10,11 @@ import {
   NavigationMenuList,
   navigationMenuTriggerStyle,
 } from "@/components/ui/navigation-menu"
-import { Avatar } from "@/components/ui/avatar"
-import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover"
-import { Button } from "./ui/button"
-import { SlLogout } from "react-icons/sl"
-
-//fix
-const mail = "castro.benjamin@gmail.com"
-const placeholder = "BC";//acá en realidad toma las iniciales del primer ombre y primer apellido del usuario actual
-const nombre = "Benjamin" + " " + "Castro";
-
 
 export function NavigationMenuDemo() {
-    return (
+  return (
     <NavigationMenu viewport={false} className="mx-auto mt-4 py-5">
-      <NavigationMenuList className="flex flex-col sm:flex-row sm:flex-wrap gap-4">
-        <Popover> 
-          <PopoverTrigger><Avatar>{placeholder}</Avatar></PopoverTrigger>
-          <PopoverContent className="bg-foreground text-background text-sm mt-2">
-            <p>{mail}</p>
-            <Avatar className="bg-card align-middle hover:bg-card">{placeholder}</Avatar>
-            <p className="text-lg p-1 border-b-1 border-accent">{nombre}</p>
-            <Button>Log Out <SlLogout /></Button>
-          </PopoverContent>
-        </Popover>
-        
+      <NavigationMenuList className="flex gap-4">
         <NavigationMenuItem>
           <NavigationMenuLink asChild className={navigationMenuTriggerStyle()}>
             <Link href="/homePage">Página Principal</Link>
