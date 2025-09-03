35a8a06392dc0d0b70bd5442f870cd8f0c331e4e
Sebas <sebastian.deleon.et32@gmail.com>
2025-09-03 15:01:47 -0300

    prueba

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+"use client";
+
 import Image from "next/image";
+import { useRouter } from "next/navigation";
 
 export default function Home() {
+  const router = useRouter();
+
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
+    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
         <Image
           className="dark:invert"
@@ -12,11 +17,11 @@ export default function Home() {
           height={38}
           priority
         />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
+        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2 tracking-[-.01em]">
             Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              src/app/page.tsx
+            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
+              app/page.tsx
             </code>
             .
           </li>
@@ -24,6 +29,22 @@ export default function Home() {
             Save and see your changes instantly.
           </li>
         </ol>
+
+        <div className="flex gap-4 mt-6">
+
+          <button
+            onClick={() => router.push("/main")}
+            className="px-6 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition">
+            Ir a main
+          </button>
+
+          <button
+            onClick={() => router.push("/login")}
+            className="px-6 py-2 rounded-md bg-blue-600 text-white hover:bg-blue-700 transition">
+            Ir a login
+          </button>
+
+        </div>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
@@ -95,7 +116,6 @@ export default function Home() {
             width={16}
             height={16}
           />
-          Go to nextjs.org →
         </a>
       </footer>
     </div>
