cfff342462a70d35277b7d65be5a87854027f1aa
NicolasLopezET32 <nicolasandrelopezarias2006@gmail.com>
2025-10-28 15:41:29 -0300

    Cambios en Componentes
    Se cambios algunos imports en los iconos de los componentes para sean iguales a los que usamos siempre

--- src/app/gestionar_descuentos/page.tsx
+++ src/app/gestionar_descuentos/page.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 import { Button } from "@/components/ui/button";
 import { Popover, PopoverTrigger, PopoverContent } from "@/components/ui/popover";
 import { Search } from "@/components/ui/search";
-
 import { SlArrowDown, SlMenu } from "react-icons/sl";
 import { DescuentoList } from "@/components/descuentoList";
+
 
 export default function DiscountManagement() {
   const [sortBy, setSortBy] = useState<"nombre" | "valor" | "fecha_inicio">("nombre");
@@ -13,19 +13,17 @@ export default function DiscountManagement() {
   const [searchTerm, setSearchTerm] = useState("");
 
   return (
-    <main className="bg-foreground rounded-xl shadow-lg p-6 sm:p-6 2xl:max-w-450 sm:max-w-200 m-auto flex flex-col items-center justify-center">
-      <h2 className="text-background text-center text-3xl font-bold mb-6">DESCUENTOS</h2>
-
+    <main className="bg-var5 dark:bg-var2 rounded-xl shadow-lg p-6 sm:p-6 2xl:max-w-450 sm:max-w-200 m-auto flex flex-col items-center justify-center">
+      <h2 className="text-foreground text-center text-3xl font-bold mb-6 ">DESCUENTOS</h2>
       <div className="flex flex-col sm:flex-row w-full gap-4">
         <Search id="search-product" placeholder="Buscar" className="w-full flex-1 min-w-0" label="" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} />
-        {/* Ordenar por */}
         <Popover>
           <PopoverTrigger>
             <Button>
-              <SlMenu /> Ordenar por <SlArrowDown />
+              <SlMenu /> Ordenar por <SlArrowDown/>
             </Button>
           </PopoverTrigger>
-          <PopoverContent className="w-48 gap-1 flex flex-col items-start" align="center">
+          <PopoverContent className="w-48 gap-1 flex flex-col items-start bg-var2 drop-shadow-2xl/75" align="center">
             <button onClick={() => setSortBy("nombre")}className="text-left cursor-pointer">Nombre</button>
             <button onClick={() => setSortBy("valor")}className="text-left cursor-pointer">Valor</button>
             <button onClick={() => setSortBy("fecha_inicio")}className="text-left cursor-pointer">Fecha inicio</button>
@@ -33,7 +31,6 @@ export default function DiscountManagement() {
           </PopoverContent>
         </Popover>
       </div>
-      {/* Pasamos el orden como props */}
       <DescuentoList sortBy={sortBy} sortOrder={sortOrder} searchTerm={searchTerm}/>
     </main>
   );
